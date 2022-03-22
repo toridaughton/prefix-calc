b6b5ddb49178d4baf54b094c1450493f06862f5f
Tori Daughton <102088965+toridaughton@users.noreply.github.com>
2022-03-22 16:43:31 -0600

    Commit Message
    Functioning Prefix Calculator for: addition, subtraction, multiplication, division, square root.

---
+++ calc.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+const { ReadableStreamBYOBRequest } = require('stream/web');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate? ", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+    console.log("mathSymbol", mathSymbol);
+    console.log("num1", num1);
+    console.log("num2", num2);
+
+    if (mathSymbol === "+"){
+        console.log(num1 + num2);
+    }
+
+    else if (mathSymbol === "-"){
+      console.log(num1 - num2);
+    }
+    else if (mathSymbol === "/"){
+      console.log(num1 / num2);
+    }
+
+    else if (mathSymbol === "*") {
+      console.log(num1 * num2);
+    }
+
+    else if (mathSymbol === "root") {
+      console.log(Math.sqrt(num1), " and ", Math.sqrt(num2));
+    }
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
